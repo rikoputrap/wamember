98afe32725fb3b7227276d9a9b684caa94e0bece
Riko Putra <rikopp27@gmail.com>
2021-07-07 18:57:44 +0700

    laravel breeze auth install

--- laraadmin/routes/web.php
+++ laraadmin/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
