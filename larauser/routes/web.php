18edb7ebbb4c48f86ea14c7b4d4e2146f861158d
mohammadrafly <44152125+mohammadrafly@users.noreply.github.com>
2021-07-08 07:21:09 +0700

    Patch v1.1.1

---
+++ larauser/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/member', function () {
+    return view('member.index');
+})->name('member');
+
+Route::post('/member', function (\Illuminate\Http\Request $request) {
+    $name = $request->input('name');
+    $address = $request->input('address');
+    $basephone = env('APP_BASEPHONE', '6281907861308');
+    return redirect('https://api.whatsapp.com/send?phone={' . $basephone  . '}&text=' . '!daftar@' . $name . '@' . $address);
+})->name('member');
